416cd1a6dc8c97b10109ce227b4ca5174cc8b5a8
ArthurFerreiraRodrigues <arthur.250402@gmail.com>
2025-11-22 17:36:36 -0300

    #3 Importando modelos do Core

--- src/nf_scanner_api/models/__init__.py
+++ src/nf_scanner_api/models/__init__.py
@@ -1,1 +1,6 @@
 """Modelos para a API do NF Scanner."""
+
+from nf_scanner_api.models.document import Document
+from nf_scanner_api.models.nfse import NFSe
+
+__all__ = ["Document", "NFSe"]
--- src/nf_scanner_api/models/document.py
+++ src/nf_scanner_api/models/document.py
@@ -1,10 +1,22 @@
 """Modelo de documento para a API do NF Scanner."""
 
 class Document:
-    """Modelo que representa um documento processado."""
-    
-    def __init__(self, document_id: str, content: dict):
-        """ ** Placeholder para criação da arquitetura
+    """Modelo que representa um documento recebido."""
+
+    def __init__(self, document_name: str, base64_content: str):
+        """Construtor do modelo de documento.
+
+        Args:
+            document_name: Nome do documento
+            base64_content: Conteúdo do documento em base64
         """
-        self.document_id = document_id
-        self.content = content
+        self.document_name = document_name
+        self.base64_content = base64_content
+
+    def to_dict(self) -> dict:
+        """Converte o modelo de documento para um dicionário."""
+        return {
+            "document_name": self.document_name,
+            "base64_content": self.base64_content,
+        }
+
